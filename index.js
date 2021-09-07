e71e9375a4f2e909f8f149b56ed74a00ae4de33e
Jenny Kim <kimj1905@gmail.com>
2021-09-07 15:06:50 -0400

    finished 'a' and 'b'

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const baseURL = "https://pokeapi.co/api/v2/pokemon?limit=50&offset=200"
+
+fetch(baseURL)
+    .then(resp => resp.json())
+    .then(pokemon => getPokemon(pokemon.results))
+
+function getPokemon(pokemon) {
+    // debugger
+    const list = document.getElementById("list")
+    for (monster of pokemon) {
+        const li = document.createElement("li")
+        li.innerHTML = monster.name
+        list.append(li)
+        li.addEventListener("mouseover", letsHover)
+        // debugger
+        fetch(monster.url)
+            .then(resp => resp.json())
+            // .then(data => getMoreInfo(data))
+            .then(data => { debugger })
+        // li.addEventListener("click", getMoreInfo)
+        // debugger
+        li.addEventListener("click", function (e) {
+            debugger
+        })
+    }
+}
+
+function letsHover(e) {
+    e.target.style.color = "red"
+}
+
+function getMoreInfo(data) {
+    // fetch(monster.url)
+    // .then(resp => resp.json())
+    // .then(data => getMoreInfo(data))
+    debugger
+}
